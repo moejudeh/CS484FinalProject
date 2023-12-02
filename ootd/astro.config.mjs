e0ae484ee54e7261c749c03612da147f25ed6461
moejudeh <122400100+moejudeh@users.noreply.github.com>
2023-12-02 15:07:21 -0600

    fixed config file

--- ootd/astro.config.mjs
+++ ootd/astro.config.mjs
@@ -1,20 +1,22 @@
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
-
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "hybrid",
   server: {
     port: 3000,
   },
+  integrations: [react()],
   vite: {
     optimizeDeps: {
       exclude: ["crypto"],
     },
   },
   adapter: netlify(),
-  ssr: true,
-  integrations: [react()],
+  adapter: node({
+    mode: "standalone",
+  }),
 });
